7fc044367eca2f9073364d248ff3f9b646e8181b
unknown <info@ameste.se>
2021-05-03 13:53:36 +0200

    setting up vue-structure

---
+++ src/components/LoginBox.vue
@@ -0,0 +1,25 @@
+<template>
+  <form>
+    <label for="email">E-post:</label><br />
+    <input type="email" name="email" id="email" /><br />
+    <label for="password">Lösenord:</label><br />
+    <input type="password" name="password" id="password" /><br />
+    <button type="submit" class="button">Login</button>
+  </form>
+</template>
+
+
+<script>
+export default {
+  name: "LoginBox",
+  props: {
+    msg: String,
+  },
+  methods: {
+
+  }
+};
+</script>
+
+<style scoped lang="scss">
+</style>

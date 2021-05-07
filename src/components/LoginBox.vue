9041df089a68c0bdee5a1e7d173f23e12f15b4aa
unknown <info@ameste.se>
2021-05-07 09:17:55 +0200

    loginpage

--- src/components/LoginBox.vue
+++ src/components/LoginBox.vue
@@ -1,24 +1,86 @@
 <template>
   <div>
-    <form>
-      <label for="email">E-post:</label><br />
-      <input type="email" name="email" id="email" /><br />
-      <label for="password">Lösenord:</label><br />
-      <input type="password" name="password" id="password" /><br />
-      <button type="submit" class="button">Login</button>
+    <form
+      id="app"
+      @submit="checkForm"
+      action="https://vuejs.org/"
+      method="post"
+    >
+     <p v-if="errors.length">
+      <b>Du måste fylla i formuläret enligt nedan:</b>
+      <ul>
+        <li v-for="error in errors" v-bind:key="error.idx">{{ error }}</li>
+      </ul>
+    </p>
+    <p>
+        <label for="email">E-post:</label>
+        <input id="email" v-model="email" type="email" name="email">
+    </p>
+    <p>
+        <label for="password">Lösenord</label>
+        <input id="password" v-model="password" type="password" name="password">
+    </p>
+      <button  type="submit" value="Submit" class="button">Login</button>
     </form>
-    <p>Inget konto?</p><router-link to="register">Registrera här!</router-link>
+    <p>Inget konto?</p>
+    <router-link to="register">Registrera här!</router-link>
+    <br />
   </div>
 </template>
 
 
 <script>
+import axios from "axios";
 export default {
   name: "LoginBox",
   props: {
     msg: String,
   },
-  methods: {},
+  data() {
+    return {
+      password: null,
+      email: null,
+      errors: [],
+      info: null,
+    };
+  },
+
+  methods: {
+    checkForm(e) {
+      if (this.name && this.age) {
+        return true;
+      }
+
+      this.errors = [];
+     
+      if (!this.email) {
+        this.errors.push("Fyll i en e-post.");
+      }
+    
+      if (!this.password) {
+        this.errors.push(
+          "Ange ditt lösenord."
+        );
+      }
+
+      e.preventDefault();
+
+      if (!this.errors.length) {
+        this.login();
+      }
+    },
+    login() {
+      axios
+        .post("http://localhost:3000/user/login", {
+          email: this.email,
+          password: this.password,
+        })
+        .then((response) => {
+          (this.info = response.data),
+            (localStorage.token = response.data.token);
+        });
+    },
+  },
 };
 </script>
 

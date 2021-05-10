2596b016b3dce2e4d16f9ef1cd7bdb9a2be3f0de
unknown <info@ameste.se>
2021-05-10 10:30:00 +0200

    contactform

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,6 +1,44 @@
 <template>
   <div class="about">
+    <form>
+      <label for="name">Namn</label>
+      <input id="name" v-model="name" type="text" name="name" />
+      <label for="email">E-post:</label>
+      <input id="email" v-model="email" type="email" name="email" />
+      <label for="msg">Meddelande</label>
+      <textarea id="msg" v-model="msg" name="msg"></textarea>
+      <button class="button" type="submit"></button>
+    </form>
     <h1>KOntakformulär och grejjer.</h1>
-<h2>KOM IHÅG GDPR_RUTA HÄR OCKSÅ</h2>
+    <h2>KOM IHÅG GDPR_RUTA HÄR OCKSÅ</h2>
   </div>
 </template>
+
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      info: null,
+      errors: [],
+      msg: null,
+      name: null,
+      mail: null,
+    };
+  },
+  methods: {
+    sendContactMail() {
+      axios.post(
+        "http://localhost:3000/contactmail",
+        {
+          name: this.name,
+          mail: this.mail,
+          msg: this.msg
+        },
+      );
+    },
+  },
+};
+</script>
